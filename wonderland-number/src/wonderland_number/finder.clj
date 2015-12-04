5f2583e0701c7f2175304aec2c5713d055ca0b75
myriamTr <myriam.truffert@gmail.com>
2015-12-04 16:24:41 +0100

    Update solution for wonderland-number

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,19 @@
 (ns wonderland-number.finder)
 
+;; - It has six digits
+;; - If you multiply it by 2,3,4,5, or 6, the resulting number has all
+;;   the same digits in at as the original number.  The only difference
+;;   is the position that they are in.
+
+(def candidates
+  (take-while #(< % (Math/pow 10 6)) (drop (Math/pow 10 5) (range))))
+
+(defn same-digits [x y]
+  (apply = (map (comp set str) [x y])))
+
+(defn valid? [x]
+  (every? identity (map #(same-digits x (* x %)) (range 2 7))))
+
 (defn wonderland-number []
   ;; calculate me
-  42)
+  (first (filter valid? candidates)))
